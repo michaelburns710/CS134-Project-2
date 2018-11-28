f4b6e9fec5ea76230605ec81358559cc4e0c337a
micha <micha@DESKTOP-PABCBJ8>
2018-11-28 15:50:13 -0600

    update 2

---
+++ Project 2 CS134/src/Menu.java
@@ -0,0 +1,19 @@
+
+public class Menu {
+	
+	public void mainMenu() {
+		System.out.println("##################################################################");
+		System.out.println("What would you like to do?");
+		System.out.println("(1) Hire New Employees");
+		System.out.println("(2) Logout");
+		
+	}
+	
+	public void employeeOption() {
+		System.out.println("What type of employee would you like to hire?");
+		System.out.println("(1) Salary");
+		System.out.println("(2) Hourly");
+		System.out.println("(3) Exit");
+		
+	}
+}
